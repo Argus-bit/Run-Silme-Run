d522f66a6a64e65205ebbf037ffe021dee419eaf
Felipe Farias <felipeo.farias@hotmail.com>
2022-07-21 10:46:10 -0300

    Atualização 21.07.2022

--- Assets/Scripts/Plataform.cs
+++ Assets/Scripts/Plataform.cs
@@ -8,11 +8,24 @@ public class Plataform : MonoBehaviour
     public float timeToReset; /*Variável para resetar o tempo de uso do Prefab*/
     public Rigidbody2D rig; /*Variável do movimento da plataforma.*/
     public float speed;
+    private float timeCount;
     //Variáveis
+
+    private void Update()
+    {
+        timeCount += Time.deltaTime;
+        if (timeCount >= 10)
+        {
+            speed += 0.02f;
+            timeCount = 0;
+        }
+    }
     void FixedUpdate()
     {
 
+
         rig.MovePosition(transform.position + transform.up * speed);
+
     }
 
     public void StartPlat()
